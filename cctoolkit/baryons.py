ab8c564d87c42252fee492455665aeb1fe346062
Tiago Castro <tiagobscastro@gmail.com>
2024-08-06 21:42:05 +0200

    Adding documentation to the baryons module

--- cctoolkit/baryons.py
+++ cctoolkit/baryons.py
@@ -102,31 +102,107 @@ def compute_dmo_mass(M_vir_hydro, z, fb_cosmic, relation='magneticum'):
 
     return M_Delta_dmo, Delta_over_Delta_vir
 
-def concentration_duffy_et_al_2008 (M, z):
+def concentration_duffy_et_al_2008(M, z):
+    """
+    Calculate the concentration-mass relation according to Duffy et al. (2008) for virial masses.
 
-    Avir = 7.85 
+    This function implements the formula provided by Duffy et al. (2008) for determining
+    the concentration parameter as a function of mass and redshift. The formula is given by:
+
+    .. math::
+
+        c(M, z) = A_{vir} \\left(\\frac{M}{M_{piv}}\\right)^{B_{vir}} (1+z)^{C_{vir}}
+
+    where the parameters are defined as follows:
+    - :math:`A_{vir} = 7.85`
+    - :math:`B_{vir} = -0.081`
+    - :math:`C_{vir} = -0.71`
+    - :math:`M_{piv} = 2 \\times 10^{12} M_\odot`
+
+    Parameters
+    ----------
+    M : float
+        Virial mass of the halo in solar masses.
+    z : float
+        Redshift at which the concentration is calculated.
+
+    Returns
+    -------
+    float
+        Concentration parameter for the given mass and redshift.
+
+    References
+    ----------
+    Duffy, A. R., et al. (2008). "Dark matter halo concentrations in the Wilkinson
+    Microwave Anisotropy Probe year 5 cosmology." Monthly Notices of the Royal
+    Astronomical Society, 390(2), L64-L68.
+    """
+    Avir = 7.85
     Bvir = -0.081
     Cvir = -0.71
     Mpv  = 2e12
 
     return Avir * (M/Mpv) ** Bvir * (1+z)**Cvir
 
-def compute_rec_mass(cosmo, M_Delta_dmo, Delta, z, c=concentration_duffy_et_al_2008):
 
+def compute_rec_mass(cosmo, M_Delta_dmo, Delta, z, c=concentration_duffy_et_al_2008):
+    """
+    Reconstruct the dark matter-only (DMO) mass of a hydro object using the model
+    presented in [arxiv:2311.01465](https://arxiv.org/pdf/2311.01465).
+
+    This function calculates the reconstructed mass of a hydro object, denoted as
+    \( M_{\Delta,\text{dmo}} \), using the provided concentration-mass relation
+    and cosmological parameters.
+
+    Parameters
+    ----------
+    cosmo : object
+        Cosmology object containing cosmological parameters and methods to compute
+        relevant quantities, such as the critical density and Omega_m(z).
+    M_Delta_dmo : float
+        Mass of the dark matter-only object within radius defined by \(\Delta\) times
+        the critical density, in solar masses.
+    Delta : float
+        Overdensity parameter, defining the radius within which the mass
+        \( M_{\Delta,\text{dmo}} \) is calculated.
+    z : float
+        Redshift at which the calculation is performed.
+    c : function, optional
+        Function to compute the concentration parameter given mass and redshift.
+        Default is `concentration_duffy_et_al_2008`.
+
+    Returns
+    -------
+    float
+        Reconstructed virial mass of the object at redshift `z`.
+
+    Notes
+    -----
+    The function first computes the virial overdensity parameter, then iteratively
+    solves for the virial radius \( R_{\text{vir}} \) using the auxiliary NFW
+    function. The virial mass is finally obtained using the computed radius and
+    density.
+
+    References
+    ----------
+    - [arxiv:2311.01465](https://arxiv.org/pdf/2311.01465): "Euclid preparation XXXIX. The effect of baryons on the halo mass function",
+      Castro et al., 2024.
+
+    """
     R_Delta_dmo = np.cbrt(M_Delta_dmo / (4*np.pi*critical_density0*Delta/3))
     Delta_vir = virial_Delta(cosmo.Omega_m(z))
-    
+
     # Auxiliary NFW function
     f = lambda x, _c: np.log(1+_c*x) - _c*x/(1+_c*x)
     # Auxiliary function to be solved
-    def fRvir (R): 
-        
+    def fRvir (R):
+
         Mvir = 4 * np.pi/3 * R**3 * critical_density0 * Delta_vir
         cvir = c(Mvir, z)
         c_Delta = R_Delta_dmo/R * cvir
-        
+
         return M_Delta_dmo * f(R/R_Delta_dmo, c_Delta) / f(1, c_Delta) / (4*np.pi/3*critical_density0*R**3) - Delta_vir
-    
+
     Rvir = fsolve(fRvir, 0.99 * R_Delta_dmo)[0]
-    
+
     return (4*np.pi/3) * Rvir**3 * critical_density0 * Delta_vir
--- docs/source/examples.rst
+++ docs/source/examples.rst
@@ -42,7 +42,7 @@ Calculate the halo mass function for a range of masses:
 Halo Bias
 ---------
 
-Compute the linear halo bias or the at redshift z = 0 for a given mass. CCToolkit can compute both the PBS prescription as well as the corrected model following Castro et al. 2024.
+Compute the linear halo bias or the at redshift z = 0 for a given mass. CCToolkit can compute both the PBS prescription as well as the corrected model following Castro et al. 2024a.
 
 .. code-block:: python
 
@@ -55,3 +55,23 @@ Compute the linear halo bias or the at redshift z = 0 for a given mass. CCToolki
    plt.legend()
    plt.show()
 
+Impact of Baryons on Cluster and Groups masses
+----------------------------------------------
+
+Compute the dark-matter only equivalent of a hydro-dynamic simulated object. CCToolkit can compute both the dark-matter only mass at the threshold of equivalence according to the quasi-adibatic model presented in Castro et al. 2024b but also to convert this to the virial definition assuming a concentration mass relation. For any realistic feedback mechanism, the last step is not very sensitive to the assumed mass-concentration relation. The assumed mass-concentration relation is given by Duffy et al. 2008.
+
+.. code-block:: python
+
+    from cctoolkit import baryons
+    from cctoolkit.cosmology import CosmologyCalculator
+    z = 0.0
+    # Magneticum cosmology
+    params = {"Om0": 0.272, "Ob0": 0.272 * 0.168, "H0": 70.4, "ns": 0.963, "mnu":0, "num_massive_neutrinos": 0, "sigma8": 0.809}
+    cosmo_calc = CosmologyCalculator()
+    # Array of virial masses on hydro
+    M = np.geomspace(1e13, 3e14)
+    # Calculating the equivalent DMO mass at the threshold of equivalence according to the quasi-adiabatic model
+    M_Delta_dmo, Delta = baryons.compute_dmo_mass(M, z, 0.168)
+    Delta *= cctoolkit.utils.virial_Delta(cosmo_calc.Omega_m(z))
+    # Converting to the virial mass
+    mdmo = [baryons.compute_rec_mass(cosmo_calc, m, d, z) for m, d in zip(M_Delta_dmo, Delta)]
