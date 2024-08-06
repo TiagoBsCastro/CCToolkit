525a14e165a738b087b4ca70db1618f39fe674bc
TiagoBsCastro <tiagobscastro@gmail.com>
2024-08-06 18:49:42 +0200

    Fixing baryonic test

--- test_baryons.py
+++ test_baryons.py
@@ -10,4 +10,4 @@ calculator_camb = CosmologyCalculator()
 
 M_Delta_dmo, Delta = baryons.compute_dmo_mass(1e14, z, 0.168)
 Delta *= cctoolkit.utils.virial_Delta(calculator_camb.Omega_m(z))
-baryons.compute_rev_mass(calculator_camb, M_Delta_dmo, Delta, z)
+baryons.compute_rec_mass(calculator_camb, M_Delta_dmo, Delta, z)
